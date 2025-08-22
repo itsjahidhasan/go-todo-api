ac94c47e67f78564c82db02a2e6b1caf3d2dd3d0
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2025-08-23 03:25:19 +0600

    console style added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/common-nighthawk/go-figure"
+	"github.com/fatih/color"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -37,6 +39,17 @@ func main() {
 	// Routes
 	router.SetupRoutes(r)
 
-	log.Println("🚀 Server running on http://localhost:8080")
+		// --- Fancy banner & console log ---
+	banner := figure.NewFigure("GO TODO API", "", true)
+	banner.Print()
+	cyan := color.New(color.FgCyan).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	bold := color.New(color.Bold).SprintFunc()
+
+	log.Println()
+	log.Println(bold("🚀 Go Todo API is running!"))
+	log.Println(cyan("👉 Base URL: ") + green("http://localhost:8080"))
+	log.Println(cyan("📂 Swagger Docs: ") + bold(green("http://localhost:8080/docs/")))
+	log.Println()
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,11 @@ go 1.25.0
 require github.com/go-chi/chi/v5 v5.1.0 // or latest
 
 require github.com/go-chi/cors v1.2.2
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
